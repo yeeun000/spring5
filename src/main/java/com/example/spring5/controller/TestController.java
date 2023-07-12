492615edcf0470066bda57a1c9d8a515cdc14a46
이예은 <dldpdms0000@naver.com>
2023-07-12 17:51:15 +0900

    FEAT TestController 추가

---
+++ src/main/java/com/example/spring5/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.example.spring5.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String getHello() {
+        return "hi";
+    }
+
+}
