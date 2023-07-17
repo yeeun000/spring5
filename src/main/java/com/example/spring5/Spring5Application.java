57b79bd492e61027817e7b6b75c08a94d9f179d7
yeeun <dldpdms0000@naver.com>
2023-07-17 14:46:54 +0900

    2장

--- src/main/java/com/example/spring5/Spring5Application.java
+++ src/main/java/com/example/spring5/Spring5Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Spring5Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Spring5Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5Application.class, args);
+    }
 
 }
